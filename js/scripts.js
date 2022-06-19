c968ec021288dd4dc13a53805f379a9d5128fb8c
sihnrila <52959210+sihnrila@users.noreply.github.com>
2022-06-19 15:33:21 +0900

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,150 @@
+/*
+   
+    Template Name : Rolling - Freelancer Portfolio Template
+    Author : UiPasta Team
+    Website : https://www.uipasta.com/
+    Support : https://www.uipasta.com/contact-us/
+	
+	
+*/
+
+
+
+/*
+   
+   Table Of Content
+   
+   2. Smooth Scroll
+   3. Scroll Naviagation Background Change with Sticky Navigation
+   4. Mobile Navigation Hide or Collapse on Click
+   5. Scroll To Top
+   6. Typed.js
+   7. Parallax Background
+  10. Testimonial Carousel/Slider
+  11. Statistics Counter
+ 
+
+*/
+
+
+(function ($) {
+    'use strict';
+
+    jQuery(document).ready(function () {
+
+		
+       /* Smooth Scroll */
+
+        $('a.smoth-scroll').on("click", function (e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+		
+
+
+       
+       /* Scroll Naviagation Background Change with Sticky Navigation */
+		 
+        $(window).on('scroll', function () {
+            if ($(window).scrollTop() > 100) {
+                $('.header-top-area').addClass('navigation-background');
+            } else {
+                $('.header-top-area').removeClass('navigation-background');
+            }
+        });
+		
+		
+		
+		
+       /* Mobile Navigation Hide or Collapse on Click */
+		
+        $(document).on('click', '.navbar-collapse.in', function (e) {
+            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+                $(this).collapse('hide');
+            }
+        });
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        
+		 });
+		 
+		
+		
+		
+        /* Scroll To Top */
+
+
+		
+		
+        /* Typed.js */
+		
+        $(window).load(function(){
+        $(".typing").typed({
+            strings: ["SNUDOG CARE CENTER"],    /* You can change the home section typing text from
+	                                                                                            here and do not use "&" use "and" */
+            typeSpeed: 50
+          });
+         });
+        
+		 
+        /* Parallax Background */
+
+        $(window).stellar({
+            responsive: true,
+            horizontalScrolling: false,
+            hideDistantElements: false,
+            horizontalOffset: 0,
+            verticalOffset: 0,
+        });
+
+        
+	   
+		 
+        /* Testimonial Carousel/Slider */
+
+        $(".testimonial-carousel-list").owlCarousel({
+            items: 1,
+            autoPlay: true,
+            stopOnHover: false,
+            navigation: true,
+            navigationText: ["<i class='fa fa-long-arrow-left fa-2x owl-navi'></i>", "<i class='fa fa-long-arrow-right fa-2x owl-navi'></i>"],
+            itemsDesktop: [1199, 1],
+            itemsDesktopSmall: [980, 1],
+            itemsTablet: [768, 1],
+            itemsTabletSmall: false,
+            itemsMobile: [479, 1],
+            autoHeight: true,
+            pagination: false,
+            transitionStyle : "backSlide"
+        });
+		
+		
+		 $('.panel-collapse').on('show.bs.collapse', function () {
+    $(this).siblings('.panel-heading').addClass('active');
+  });
+
+  $('.panel-collapse').on('hide.bs.collapse', function () {
+    $(this).siblings('.panel-heading').removeClass('active');
+  });
+  
+		
+        /* Statistics Counter */
+		
+        $('.statistics').appear(function() {
+           var counter = $(this).find('.statistics-count');
+           var toCount = counter.data('count');
+      
+           $(counter).countTo({
+           from: 0,
+           to: toCount,
+           speed: 5000,
+           refreshInterval: 50
+           })
+           });
+        });
+
+   })(jQuery);
